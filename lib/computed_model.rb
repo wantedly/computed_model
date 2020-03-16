d40e715c8b279545fdca5829bf520ea97657750d
Masaki Hara <ackie.h.gmai@gmail.com>
2020-03-16 10:54:35 +0900

    bundle gem computed_model

---
+++ lib/computed_model.rb
@@ -0,0 +1,6 @@
+require "computed_model/version"
+
+module ComputedModel
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/computed_model/version.rb
@@ -0,0 +1,3 @@
+module ComputedModel
+  VERSION = "0.1.0"
+end
