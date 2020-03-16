d221a88f8a0d50d0fdc861648796bbd8a8f4e32f
Masaki Hara <ackie.h.gmai@gmail.com>
2020-03-16 16:48:13 +0900

    Expand docs

--- lib/computed_model.rb
+++ lib/computed_model.rb
@@ -3,19 +3,71 @@
 require "computed_model/version"
 require 'set'
 
+# A mixin for batch-loadable compound models.
+#
+# @example typical structure of a computed model
+#   class User
+#     include ComputedModel
+#
+#     attr_reader :id
+#     def initialize(id)
+#       @id = id
+#     end
+#
+#     define_loader do ... end
+#
+#     dependency :foo, :bar
+#     computed def something ... end
+#   end
 module ComputedModel
+  # An error raised when you tried to read from a loaded/computed attribute,
+  # but that attribute isn't loaded by the batch loader.
   class NotLoaded < StandardError; end
 
+  # A return value from {ComputedModel::ClassMethods#computing_plan}.
   Plan = Struct.new(:load_order, :subdeps_hash)
 
+  # A set of class methods for {ComputedModel}. Automatically included to the
+  # singleton class when you include {ComputedModel}.
   module ClassMethods
-    # @param deps [Array]
+    # Declares the dependency of a computed attribute. See {#computed} too.
+    #
+    # @param deps [Array<Symbol, Hash{Symbol=>Array}>]
+    #   Dependency description. If a symbol `:foo` is given,
+    #   it's interpreted as `{ foo: [] }`.
+    #   When the same symbol occurs multiple times, the array is concatenated.
+    #   The contents of the array (called "sub-dependency") is treated opaquely
+    #   by the `computed_model` gem. It is up to the user to design the format
+    #   of sub-dependencies.
+    # @return [void]
+    #
+    # @example declaring dependencies
+    #   dependency :user, :user_external_resource
+    #   computed def something
+    #     # Use user and user_external_resource ...
+    #   end
+    #
+    # @example declaring dependencies with sub-dependencies
+    #   dependency user: [:user_names, :premium], user_external_resource: [:received_stars]
+    #   computed def something
+    #     # Use user and user_external_resource ...
+    #   end
     def dependency(*deps)
       @__computed_model_next_dependency ||= []
       @__computed_model_next_dependency.push(*deps)
     end
 
-    # @param meth_name [Symbol]
+    # Declares a computed attribute. See {#dependency} too.
+    #
+    # @param meth_name [Symbol] a method name to promote to a computed attribute.
+    #   Typically used in the form of `computed def ...`.
+    # @return [Symbol] passes through the argument.
+    #
+    # @example define a field which is calculated from loaded models
+    #   dependency :user, :user_external_resource
+    #   computed def something
+    #     # Use user and user_external_resource ...
+    #   end
     def computed(meth_name)
       var_name = :"@#{meth_name}"
       meth_name_orig = :"#{meth_name}_orig"
@@ -43,12 +95,25 @@ module ComputedModel
       meth_name
     end
 
-    # @param methods [Array<Symbol>]
-    # @param to [Symbol]
-    # @param allow_nil [nil, Boolean]
-    # @param prefix [nil, Symbol]
-    # @param include_subdeps [nil, Boolean]
+    # A shorthand for simple computed attributes.
+    #
+    # Use {#computed} for more complex definition.
+    #
+    # @param methods [Array<Symbol>] method names to delegate
+    # @param to [Symbol] which attribute to delegate the methods to.
+    #   This parameter is used for the dependency declaration too.
+    # @param allow_nil [nil, Boolean] If `true`,
+    #   nil receivers are is ignored, and nil is returned instead.
+    # @param prefix [nil, Symbol] A prefix for the delegating method name.
+    # @param include_subdeps [nil, Boolean] If `true`,
+    #   sub-dependencies are also included.
     # @return [void]
+    #
+    # @example delegate name from raw_user
+    #   delegate_dependency :name, to: :raw_user
+    #
+    # @example delegate name from raw_user, but expose as user_name
+    #   delegate_dependency :name, to: :raw_user, prefix: :user
     def delegate_dependency(*methods, to:, allow_nil: nil, prefix: nil, include_subdeps: nil)
       method_prefix = prefix ? "#{prefix_}" : ""
       methods.each do |meth_name|
@@ -71,10 +136,30 @@ module ComputedModel
       end
     end
 
-    # @param meth_name [Symbol]
-    # @yieldparam objects [Array]
-    # @yieldparam options [Hash]
+    # Declares a loaded attribute. See {#dependency} too.
+    #
+    # `define_loader :foo do ... end` generates a reader `foo` and a writer `foo=`.
+    # The writer is only meant to be used in the loader.
+    #
+    # The responsibility of loader is to call `foo=` for all the given objects,
+    # or set `computed_model_error` otherwise.
+    #
+    # @param meth_name [Symbol] the name of the loaded attribute.
+    # @return [void]
+    # @yield [objects, **options]
+    # @yieldparam objects [Array] The objects to preload the attribute into.
+    # @yieldparam options [Hash] A verbatim copy of what is passed to {#bulk_load_and_compute}.
     # @yieldreturn [void]
+    #
+    # @example define a loader for ActiveRecord-based models
+    #   define_loader :raw_user do |users, subdeps, **options|
+    #     user_ids = users.map(&:id)
+    #     raw_users = RawUser.where(id: user_ids).preload(subdeps).index_by(&:id)
+    #     users.each do |user|
+    #       # Even if it doesn't exist, you must explicitly assign nil to the field.
+    #       user.raw_user = raw_users[user.id]
+    #     end
+    #   end
     def define_loader(meth_name, &block)
       raise ArgumentError, "No block given" unless block
 
@@ -89,8 +174,13 @@ module ComputedModel
       attr_writer meth_name
     end
 
-    # @param objs [Array]
-    # @param deps [Array]
+    # The core routine for batch-loading.
+    #
+    # @param objs [Array] The objects to preload attributes into.
+    # @param deps [Array<Symbol, Hash{Symbol=>Array}>] A set of dependencies.
+    # @param options [Hash] An arbitrary hash to pass to loaders
+    #   defined by {#define_loader}.
+    # @return [void]
     def bulk_load_and_compute(objs, deps, **options)
       objs = objs.dup
       plan = computing_plan(deps)
@@ -170,6 +260,10 @@ module ComputedModel
     normalized
   end
 
+  # An error field to prevent {ComputedModel::ClassMethods#bulk_load_and_compute}
+  # from loading remaining attributes.
+  #
+  # @return [StandardError]
   attr_accessor :computed_model_error
 
   def self.included(klass)
